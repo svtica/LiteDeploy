5d042a7ee47f99a1b033fb4e154567ff6077185a
svtica <svtica@users.noreply.github.com>
2025-05-30 09:46:49 -0400

    Initial commit - history reset on 2025-05-30 09:46:48

---
+++ Classes/MainFormAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiteDeploy
+{
+    static public class MainFormAccess
+    {
+        static public Form1 mainForm;
+
+        static public void Init(Form1 f)
+        {
+            mainForm = f;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+LiteDeploy is an unofficial fork of Easy Deploy: https://github.com/aamay001/easy-deploy.
+
+This is a work in progress, intended to be included with LiteTask in a future feature release.
